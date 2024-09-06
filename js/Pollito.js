72484e07703f9142e6657973ddc0d048b9ff35ba
Ana Badolato <badolatoana@gmail.com>
2024-09-06 13:39:01 +0200

    lesson done

---
+++ js/Pollito.js
@@ -0,0 +1,19 @@
+class Pollito{
+  
+  constructor() {
+
+    // .todos los pollitos se crearán con estos valores
+    this.x = 50;
+    this.y = 50;
+    this.h = 35;
+    this.w = 40;
+
+    // al crear el pollito:
+
+    // 1. añadir el pollito al DOM
+    this.node = document.createElement("img")
+
+    // 2. Ajustamos sus dimensiones y posiciones
+
+  }
+}
--- js/main.js
+++ js/main.js
@@ -20,15 +20,33 @@ const gameBoxNode = document.querySelector("#game-box")
 
 
 //* FUNCIONES GLOBALES DEL JUEGO
+function startGame() {
+  console.log("Iniciando juego");
 
+  // 1. Cambiar las pantallas
+  splashScreenNode.style.display = "none";
+  gameScreenNode.style.display = "flex";
 
+  // 2. Añadir todos los elementos iniciales del juego
+  let pollitoObj = new Pollito();
+  // 3. Iniciar el intervalo de juego
+  setInterval(() => {
+    // console.log("Intervalo andando");
+    gameLoop();
+  }, Math.round(1000/60)); // 60 fps
 
+  // 4. (Opcional) Iniciaremos otrs intervalos que requiera el juego
+
+}
+
+function gameLoop() {
+  // Se ejecuta 60 veces por segundo en el intervalo principal
+}
 
 
 
 //* EVENT LISTENERS
-
-
+startBtnNode.addEventListener("click", startGame); //le pasamos la función sin () porque no la invocamos
 
 
 
